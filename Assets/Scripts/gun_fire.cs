d43364a922cad380fd1ef7c7d6967ece34d6e1d5
Daryl Fung <darylfung96@gmail.com>
2019-04-13 13:31:35 -0500

    added gunshot

---
+++ Assets/Scripts/gun_fire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun_fire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            AudioSource gunShot = GetComponent<AudioSource>();
+            gunShot.Play();
+            GetComponent<Animation>().Play();
+        }
+    }
+}
