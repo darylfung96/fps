e34e3759c039cfc9978c1e7d89c4e1f8a70b9080
Daryl Fung <darylfung96@gmail.com>
2019-04-14 02:34:33 -0500

    added door

---
+++ Assets/Scripts/open_door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class open_door : MonoBehaviour {
+
+    public GameObject textDisplay;
+    public float distanceToPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        distanceToPlayer = player_casting.distanceToTarget;
+        if (distanceToPlayer <= 2)
+        {
+            textDisplay.GetComponent<Text>().text = "Open Door";
+        }
+    }
+
+    private void OnMouseExit()
+    {
+            textDisplay.GetComponent<Text>().text = "";
+    }
+
+}
---
+++ Assets/Scripts/player_casting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_casting : MonoBehaviour {
+
+    public static float distanceToTarget;
+    public float toTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            toTarget = hit.distance;
+            distanceToTarget = toTarget;
+        }
+		
+	}
+}
